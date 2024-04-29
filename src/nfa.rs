2196c7ddae172fe1919600f9d76dd17bdbfff08a
Jakob Ristner <jakob.o.ristner@gmail.com>
2024-04-29 15:38:42 +0200

    moved code into nfa struct

--- src/nfa.rs
+++ src/nfa.rs
@@ -4,11 +4,8 @@ use std::sync::atomic::AtomicUsize;
 
 type RefState = Rc<RefCell<State>>;
 
-static ID: AtomicUsize = AtomicUsize::new(0);
-
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct State {
-    id: usize,
     accepting: bool,
     transitions: Vec<Transition>,
 }
@@ -29,11 +26,10 @@ impl State {
         Rc::new(RefCell::new(State {
             transitions: Vec::new(),
             accepting,
-            id: ID.fetch_add(1, std::sync::atomic::Ordering::SeqCst),
         }))
     }
 
-    pub fn add_transition(&mut self, symbol: Option<char>, next_state: RefState) {
+    pub fn add_trans(&mut self, symbol: Option<char>, next_state: RefState) {
         self.transitions.push(Transition { symbol, next_state });
     }
 }
@@ -42,16 +38,10 @@ impl NFA {
     pub fn new(start: RefState) -> NFA {
         NFA { start }
     }
-}
 
-pub fn run(nfa: &NFA, input: &str) -> bool {
-    let mut current_states = vec![nfa.start.clone()];
-    let mut chars = input.chars();
-
-    while let Some(c) = chars.next() {
+    fn epsilon_closure(&self, current_states: &mut Vec<RefState>) {
         let mut i = 0;
 
-        // Epsilon Closure
         while i < current_states.len() {
             let mut ep_transitions: Vec<RefState> = Vec::new();
             for tran in &current_states[i].borrow().transitions {
@@ -62,41 +52,48 @@ pub fn run(nfa: &NFA, input: &str) -> bool {
             current_states.extend(ep_transitions);
             i += 1;
         }
+    }
 
-        // Move states
+    fn step(&self, current_states: &mut Vec<RefState>, c: char) {
         let mut next_states: Vec<RefState> = Vec::new();
-        for state in &current_states {
+        for state in current_states.iter() {
             for tran in &state.borrow().transitions {
                 if tran.symbol == Some(c) {
                     next_states.push(tran.next_state.clone());
                 }
             }
         }
-        current_states = next_states;
+        current_states.clear();
+        current_states.extend(next_states);
     }
 
-    current_states.iter().any(|state| state.borrow().accepting)
+    pub fn run(&self, input: &str) -> bool {
+        let mut current_states = vec![self.start.clone()];
+        let mut chars = input.chars();
+
+        while let Some(c) = chars.next() {
+            // Epsilon Closure
+            self.epsilon_closure(&mut current_states);
+            // Move states
+            self.step(&mut current_states, c);
+            // Epsilon Closure
+            self.epsilon_closure(&mut current_states);
+        }
+        current_states.iter().any(|state| state.borrow().accepting)
+    }
 }
 
 pub fn test() {
     // create simple NFA
 
     let state1 = State::new(false);
-    let state2 = State::new(true);
+    let state2 = State::new(false);
     let state3 = State::new(true);
 
-    state1
-        .borrow_mut()
-        .add_transition(Some('a'), state2.clone());
-    state2.borrow_mut().add_transition(None, state3.clone());
-    state3
-        .borrow_mut()
-        .add_transition(Some('b'), state2.clone());
-    state2
-        .borrow_mut()
-        .add_transition(Some('x'), state3.clone());
+    state1.borrow_mut().add_trans(Some('a'), state2.clone());
+    state2.borrow_mut().add_trans(None, state3.clone());
 
     let nfa = NFA::new(state1);
-    let x = run(&nfa, "az");
+    let x = nfa.run("ax");
     println!("{}", x);
 }
